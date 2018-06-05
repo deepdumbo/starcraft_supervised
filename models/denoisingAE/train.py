eed33a8a70beb36db8668d4468de9d5ff75957f3
drlego9 <drlego9@gmail.com>
2018-06-05 18:09:34 +0900

    changed way of defining model

--- models/denoisingAE/train.py
+++ models/denoisingAE/train.py
@@ -5,7 +5,6 @@ from __future__ import division
 from __future__ import print_function
 
 import os
-import math
 
 import numpy as np
 import tensorflow as tf
@@ -16,7 +15,7 @@ from keras.callbacks import ModelCheckpoint
 from keras.optimizers import Adam
 
 from ae import denoising_AE
-from train_utils import get_available_processors, generate_arrays_from_directory
+from train_utils import generate_train_batches_from_directory, get_steps_per_epoch
 
 num_classes = 2
 batch_size = 16
@@ -26,29 +25,27 @@ shape = (128, 128, 49)
 
 if __name__ == '__main__':
 
-    # FIXME: is it L2-norm?
     base_model = denoising_AE(input_shape=shape)
-    base_model.compile(optimizer=Adam(lr=learning_rate),
-                       loss={'recon': 'mse', 'feat': 'mse'})
-
-    model = Model(inputs=[base_model.input], outputs=[base_model.outputs[-1]])
+    inputs = [base_model.input]
+    outputs = [x for x in base_model.outputs if x.name.startswith('recon')]  # use only decoder output
+    model = Model(inputs=inputs, outputs=outputs)
     model.compile(optimizer=Adam(lr=learning_rate),
                   loss='mse')
 
     # TODO: Add more callbacks
     callbacks = []
-    check_dir = 'D:/parsingData/checkpoints/'
-    if not os.path.isdir(check_dir):
-        os.makedirs(check_dir)
-    check = ModelCheckpoint(filepath=os.path.join(check_dir, 'trained_AE.h5'),
+    checkpoint_dir = 'D:/parsingData/checkpoints/'
+    if not os.path.isdir(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
+    check = ModelCheckpoint(filepath=os.path.join(checkpoint_dir, 'trained_AE.h5'),
                             monitor='val_loss',
                             save_best_only=True,
                             save_weights_only=True)
     callbacks.append(check)
 
-    datadir = 'D:/parsingData/trainingData_v4/by_sample/'
-    generator = generate_arrays_from_directory(datadir, batch_size)
-    steps_per_epoch = math.ceil(os.listdir(datadir).__len__() / batch_size)
+    data_dir = 'D:/parsingData/trainingData_v4/by_sample/'
+    generator = generate_train_batches_from_directory(data_dir, batch_size)
+    steps_per_epoch = get_steps_per_epoch(len(os.listdir(data_dir)), batch_size)
     model.fit_generator(generator=generator,
                         steps_per_epoch=steps_per_epoch,
                         epochs=epochs,
