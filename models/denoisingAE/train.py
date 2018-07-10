06f40f6e3a70a2610c69ee81d63406a82fe3ee10
drlego <drlego9@gmail.com>
2018-07-10 20:08:56 +0900

    support for valid generator

--- models/denoisingAE/train.py
+++ models/denoisingAE/train.py
@@ -10,25 +10,23 @@ import numpy as np
 import tensorflow as tf
 
 import keras
-from keras.engine.training import Model
 from keras.callbacks import ModelCheckpoint
 from keras.optimizers import Adam
+from keras.utils import multi_gpu_model
 
 from ae import denoising_AE
-from train_utils import generate_train_batches_from_directory, get_steps_per_epoch
+from train_utils import generate_batches_from_directory, get_steps_per_epoch
 
 num_classes = 2
-batch_size = 16
+batch_size = 128
 epochs = 100
 learning_rate = 0.001
 shape = (128, 128, 49)
 
 if __name__ == '__main__':
 
-    base_model = denoising_AE(input_shape=shape)
-    inputs = [base_model.input]
-    outputs = [x for x in base_model.outputs if x.name.startswith('recon')]  # use only decoder output
-    model = Model(inputs=inputs, outputs=outputs)
+    model = denoising_AE(input_shape=shape)
+    model = multi_gpu_model(model, gpus=4, cpu_merge=False)
     model.compile(optimizer=Adam(lr=learning_rate),
                   loss='mse')
 
@@ -43,12 +41,33 @@ if __name__ == '__main__':
                             save_weights_only=True)
     callbacks.append(check)
 
-    data_dir = 'D:/parsingData/trainingData_v4/by_sample/'
-    generator = generate_train_batches_from_directory(data_dir, batch_size)
-    steps_per_epoch = get_steps_per_epoch(len(os.listdir(data_dir)), batch_size)
-    model.fit_generator(generator=generator,
-                        steps_per_epoch=steps_per_epoch,
+    data_dir = 'D:/parsingData/parsingData_v4/by_sample_npy/'
+
+    total_size = len(os.listdir(data_dir))
+
+    train_start = 0
+    train_end = int(total_size * 0.8)
+    valid_start = train_end
+    valid_end = total_size
+
+    generator_train = generate_batches_from_directory(path_to_dir=data_dir,
+                                                      batch_size=batch_size,
+                                                      start=train_start,
+                                                      end=train_end)
+    generator_valid = generate_batches_from_directory(path_to_dir=data_dir,
+                                                      batch_size=batch_size,
+                                                      start=valid_start,
+                                                      end=valid_end)
+
+    steps_per_epoch_train = get_steps_per_epoch(train_end, batch_size)
+    steps_per_epoch_valid = get_steps_per_epoch(total_size - train_end, batch_size)
+
+    model.fit_generator(generator=generator_train,
+                        steps_per_epoch=steps_per_epoch_train,
                         epochs=epochs,
-                        max_queue_size=64,
+                        max_queue_size=batch_size * 4,
+                        validation_data=generator_valid,
+                        validation_steps=steps_per_epoch_valid,
                         callbacks=callbacks,
+                        workers=8,
                         verbose=1)
